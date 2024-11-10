f9facf76130c9bcc409f5488901025683d9ced89
EmeehM <120816866+EmeehM@users.noreply.github.com>
2024-11-10 14:05:52 -0300

    Last Commit
    Los valores de las variables deberian ser retocados pero los tiempos no acompañan, de igual manera seria simplemente ir haciendo el testing.

--- app.py
+++ app.py
@@ -9,11 +9,14 @@ def create_model():
         ('Dolor_Cabeza', 'Sintomas_Principales'),
         ('Dolor_Muscular', 'Sintomas_Principales'),
         ('Nauseas', 'Sintomas_Principales'),
+        ('Malestar_General', 'Sintomas_Principales'),
+        ('Sarpullido', 'Sintomas_Principales'),
         ('Sintomas_Principales', 'Diagnostico'),
         ('Dolor_Abdominal', 'Sintomas_Secundarios'),
         ('Irritabilidad', 'Sintomas_Secundarios'),
-        ('Sarpullido', 'Sintomas_Secundarios'),
+        ('Sangrado', 'Sintomas_Secundarios'),
         ('Vomito', 'Sintomas_Secundarios'),
+        ('Dificultad_Respiratoria', 'Sintomas_Secundarios'),
         ('Sintomas_Secundarios', 'Diagnostico')
     ])
 
@@ -21,25 +24,33 @@ def create_model():
     cpd_fiebre = TabularCPD(variable='Fiebre', variable_card=2, values=[[0.7], [0.3]])
     cpd_dolor_cabeza = TabularCPD(variable='Dolor_Cabeza', variable_card=2, values=[[0.8], [0.2]])
     cpd_dolor_muscular = TabularCPD(variable='Dolor_Muscular', variable_card=2, values=[[0.75], [0.25]])
+    cpd_sarpullido = TabularCPD(variable='Sarpullido', variable_card=2, values=[[0.7], [0.3]])
     cpd_nauseas = TabularCPD(variable='Nauseas', variable_card=2, values=[[0.85], [0.15]])
+    cpd_malestar_general = TabularCPD(variable='Malestar_General', variable_card=2,values=[[0.85], [0.15]])
 
     cpd_dolor_abdominal = TabularCPD(variable='Dolor_Abdominal', variable_card=2, values=[[0.9], [0.1]])
     cpd_irritabilidad = TabularCPD(variable='Irritabilidad', variable_card=2, values=[[0.8], [0.2]])
-    cpd_sarpullido = TabularCPD(variable='Sarpullido', variable_card=2, values=[[0.7], [0.3]])
+    cpd_sangrado = TabularCPD(variable='Sangrado', variable_card=2, values=[[0.7], [0.3]])
     cpd_vomito = TabularCPD(variable='Vomito', variable_card=2, values=[[0.85], [0.15]])
+    cpd_dif_resp = TabularCPD(variable='Dificultad_Respiratoria', variable_card=2, values=[[0.85], [0.15]])
 
     # CPD para síntomas principales con los valores que me diste
     cpd_sintomas_principales = TabularCPD(
         variable='Sintomas_Principales',
         variable_card=4,  # 4 posibles salidas: Ninguno, Leve, Moderado, Severo
         values=[
-            [0.9, 0.8, 0.5, 0.2, 0.8, 0.4, 0.2, 0.1, 0.5, 0.3, 0.1, 0.0, 0.2, 0.1, 0.0, 0.0],  # Ninguno
-            [0.0, 0.2, 0.3, 0.3, 0.2, 0.5, 0.5, 0.3, 0.4, 0.5, 0.4, 0.2, 0.3, 0.3, 0.2, 0.1],  # Leve
-            [0.0, 0.0, 0.2, 0.3, 0.0, 0.1, 0.2, 0.3, 0.1, 0.2, 0.3, 0.4, 0.3, 0.3, 0.4, 0.3],  # Moderado
-            [0.1, 0.0, 0.0, 0.2, 0.0, 0.0, 0.1, 0.3, 0.0, 0.0, 0.2, 0.4, 0.2, 0.3, 0.4, 0.6],  # Severo
+            #TABLA SINTOMAS INICIALES
+            #NINGUNO:
+            [1.0, 0.8, 0.8, 0.4, 0.6, 0.4, 0.3, 0.2, 0.6, 0.5, 0.5, 0.3, 0.4, 0.3, 0.3, 0.2, 0.5, 0.6, 0.6, 0.2, 0.6, 0.2, 0.3, 0.2, 0.5, 0.3, 0.3, 0.2, 0.2, 0.2, 0.2, 0.1, 0.8, 0.5, 0.4, 0.3, 0.4, 0.4, 0.3, 0.2, 0.4, 0.3, 0.3, 0.2, 0.2, 0.2, 0.1, 0.0, 0.4, 0.2, 0.3, 0.1, 0.3, 0.2, 0.2, 0.1, 0.4, 0.2, 0.2, 0.1, 0.1, 0.1, 0.1, 0.0],
+            #LEVE:
+            [0.0, 0.2, 0.2, 0.5, 0.3, 0.4, 0.4, 0.5, 0.3, 0.3, 0.3, 0.6, 0.4, 0.4, 0.5, 0.4, 0.4, 0.3, 0.3, 0.5, 0.3, 0.5, 0.4, 0.2, 0.3, 0.4, 0.4, 0.2, 0.5, 0.2, 0.2, 0.1, 0.2, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.3, 0.5, 0.4, 0.3, 0.3, 0.4, 0.3, 0.4, 0.2, 0.4, 0.4, 0.4, 0.3, 0.4, 0.3, 0.4, 0.3, 0.4, 0.3, 0.3, 0.1, 0.4, 0.2, 0.2, 0.0],
+            #MODERADO:
+            [0.0, 0.0, 0.0, 0.1, 0.1, 0.2, 0.3, 0.3, 0.1, 0.2, 0.2, 0.1, 0.2, 0.3, 0.2, 0.3, 0.1, 0.1, 0.1, 0.3, 0.1, 0.3, 0.3, 0.4, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.4, 0.0, 0.1, 0.2, 0.3, 0.2, 0.2, 0.3, 0.3, 0.1, 0.3, 0.4, 0.2, 0.3, 0.3, 0.2, 0.4, 0.2, 0.4, 0.3, 0.4, 0.3, 0.3, 0.3, 0.4, 0.2, 0.3, 0.3, 0.4, 0.4, 0.4, 0.4, 0.5],
+            #SEVERO:
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.3, 0.0, 0.3, 0.3, 0.4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.3, 0.1, 0.2, 0.3, 0.4, 0.0, 0.0, 0.0, 0.2, 0.0, 0.2, 0.1, 0.2, 0.0, 0.2, 0.2, 0.4, 0.1, 0.3, 0.3, 0.5]
         ],
-        evidence=['Fiebre', 'Dolor_Cabeza', 'Dolor_Muscular', 'Nauseas'],
-        evidence_card=[2, 2, 2, 2]
+        evidence=['Fiebre', 'Dolor_Cabeza', 'Dolor_Muscular', 'Nauseas','Malestar_General','Sarpullido'],
+        evidence_card=[2, 2, 2, 2, 2, 2]
     )
 
     # CPD para síntomas secundarios con los valores que me diste
@@ -47,13 +58,18 @@ def create_model():
         variable='Sintomas_Secundarios',
         variable_card=4,  # 4 posibles salidas: Ninguno, Leve, Moderado, Severo
         values=[
-            [1.0, 0.8, 0.8, 0.6, 0.8, 0.6, 0.6, 0.4, 0.8, 0.6, 0.6, 0.4, 0.6, 0.4, 0.4, 0.2],  # Primer grupo de valores
-            [0.0, 0.2, 0.2, 0.3, 0.2, 0.3, 0.3, 0.4, 0.2, 0.3, 0.3, 0.4, 0.3, 0.4, 0.4, 0.4],  # Segundo grupo de valores
-            [0.0, 0.0, 0.0, 0.1, 0.0, 0.1, 0.1, 0.2, 0.0, 0.1, 0.1, 0.2, 0.1, 0.2, 0.2, 0.3],  # Tercer grupo de valores
-            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1]   # Cuarto grupo de valores
+            #TABLA SINTOMAS SECUNDARIOS
+            #NINGUNO:
+            [1.0, 0.8, 0.8, 0.6, 0.8, 0.4, 0.6, 0.3, 0.8, 0.6, 0.6, 0.4, 0.5, 0.3, 0.3, 0.2, 0.8, 0.5, 0.5, 0.3, 0.5, 0.4, 0.4, 0.1, 0.5, 0.4, 0.2, 0.1, 0.3, 0.2, 0.2, 0.1],
+            #LEVE:
+            [0.0, 0.2, 0.2, 0.3, 0.2, 0.3, 0.3, 0.4, 0.2, 0.2, 0.3, 0.3, 0.3, 0.5, 0.3, 0.3, 0.2, 0.3, 0.4, 0.4, 0.3, 0.3, 0.3, 0.3, 0.3, 0.4, 0.6, 0.3, 0.4, 0.3, 0.3, 0.2],
+            #MODERADO:
+            [0.0, 0.0, 0.0, 0.1, 0.0, 0.3, 0.1, 0.3, 0.0, 0.2, 0.1, 0.3, 0.2, 0.2, 0.4, 0.4, 0.0, 0.2, 0.1, 0.3, 0.2, 0.3, 0.3, 0.3, 0.2, 0.2, 0.2, 0.3, 0.2, 0.3, 0.3, 0.3],
+            #SEVERO:
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.0, 0.0, 0.0, 0.3, 0.1, 0.2, 0.2, 0.4]
         ],
-        evidence=['Dolor_Abdominal', 'Irritabilidad', 'Sarpullido', 'Vomito'],
-        evidence_card=[2, 2, 2, 2]
+        evidence=['Dolor_Abdominal', 'Irritabilidad', 'Sangrado', 'Vomito','Dificultad_Respiratoria'],
+        evidence_card=[2, 2, 2, 2, 2]
     )
 
     # CPD para diagnóstico con los valores que me diste
@@ -61,10 +77,15 @@ def create_model():
         variable='Diagnostico',
         variable_card=4,  # 4 posibles estados: Sin enfermedad, Dengue sin signo de alarma, Dengue con signo de alarma, Dengue severo
         values=[
-            [0.9, 0.8, 0.5, 0.2, 0.8, 0.4, 0.2, 0.1, 0.5, 0.3, 0.1, 0.0, 0.2, 0.1, 0.0, 0.0],  # Estado 0: Ninguno
-            [0.0, 0.2, 0.3, 0.3, 0.2, 0.5, 0.5, 0.3, 0.4, 0.5, 0.4, 0.2, 0.3, 0.3, 0.2, 0.1],  # Estado 1: Leve
-            [0.0, 0.0, 0.2, 0.3, 0.0, 0.1, 0.2, 0.3, 0.1, 0.2, 0.3, 0.4, 0.3, 0.3, 0.4, 0.3],  # Estado 2: Moderado
-            [0.1, 0.0, 0.0, 0.2, 0.0, 0.0, 0.1, 0.3, 0.0, 0.0, 0.2, 0.4, 0.2, 0.3, 0.4, 0.6],  # Estado 3: Severo
+            #TABLA DIAGNOSTICO
+            #SIN ENFERMEDAD:
+            [0.8, 0.5, 0.3, 0.2, 0.3, 0.2, 0.2, 0.1, 0.2, 0.3, 0.1, 0.0, 0.1, 0.1, 0.0, 0.0],
+            #DENGUE SIN SIGNO DE ALARMA:
+            [0.2, 0.4, 0.5, 0.4, 0.4, 0.4, 0.4, 0.3, 0.2, 0.3, 0.3, 0.3, 0.2, 0.2, 0.1, 0.1],
+            #DENGUE CON SIGNO DE ALARMA:
+            [0.0, 0.1, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.4, 0.3, 0.4, 0.5, 0.5, 0.3, 0.4, 0.3],
+            #DENGUE SEVERO:
+            [0.0, 0.0, 0.0, 0.1, 0.0, 0.1, 0.1, 0.3, 0.2, 0.1, 0.2, 0.2, 0.2, 0.4, 0.5, 0.6]
         ],
         evidence=['Sintomas_Principales', 'Sintomas_Secundarios'],
         evidence_card=[4, 4]
@@ -72,7 +93,7 @@ def create_model():
 
     model.add_cpds(cpd_fiebre, cpd_dolor_cabeza, cpd_dolor_muscular, cpd_nauseas,
                    cpd_dolor_abdominal, cpd_irritabilidad, cpd_sarpullido, cpd_vomito,
-                   cpd_sintomas_principales, cpd_sintomas_secundarios, cpd_diagnostico)
+                   cpd_sintomas_principales, cpd_sintomas_secundarios, cpd_diagnostico,cpd_malestar_general,cpd_sangrado,cpd_dif_resp)
 
     model.check_model()
     return model
@@ -91,8 +112,12 @@ def inferir_diagnostico(sintomas):
         'Irritabilidad': sintomas['Irritabilidad'],
         'Sarpullido': sintomas['Sarpullido'],
         'Vomito': sintomas['Vomito'],
+        'Sangrado' : sintomas['Sangrado'],
+        'Malestar_General' : sintomas['Malestar_General'],
+        'Dificultad_Respiratoria' : sintomas['Dificultad_Respiratoria'],
     }
     
     # Realizar la inferencia
     resultado = infer.query(variables=['Diagnostico'], evidence=sintomas_dict, show_progress=True)
+    print(resultado)
     return resultado
--- gui.py
+++ gui.py
@@ -1,25 +1,23 @@
 import tkinter as tk
 import customtkinter as ctk
-from fontawesome import icons  # Importar FontAwesome para los iconos
 from app import inferir_diagnostico  # La función que hace la inferencia
 
 # Configuración de la ventana principal
-ctk.set_appearance_mode("System")  # Modo claro/oscuro según configuración del sistema
-ctk.set_default_color_theme("green")  # Tema verde
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("green")
 
-root = ctk.CTk()  # Crear la ventana principal
-root.geometry("800x600")  # Establecer el tamaño de la ventana
-root.minsize(800, 600)  # Definir el tamaño mínimo
+root = ctk.CTk()
+root.geometry("600x400")  # Tamaño más compacto
+root.minsize(600, 400)
 
-# Título de la ventana
-title_label = ctk.CTkLabel(root, text="Salud Rural Inteligente", font=("Arial", 30, "bold"))
-title_label.place(x=200, y=20)  # Posicionar el título
+# Título y subtítulo
+title_label = ctk.CTkLabel(root, text="Salud Rural Inteligente", font=("Arial", 24, "bold"))
+title_label.place(x=150, y=10)
 
-# Subtítulo de la ventana
-subtitle_label = ctk.CTkLabel(root, text="Realiza tu consulta de DENGUE", font=("Arial", 20))
-subtitle_label.place(x=230, y=70)  # Posicionar el subtítulo
+subtitle_label = ctk.CTkLabel(root, text="Realiza tu consulta de DENGUE", font=("Arial", 16))
+subtitle_label.place(x=170, y=50)
 
-# Función para obtener los resultados y hacer la inferencia
+# Función para obtener el resultado del diagnóstico
 def obtener_resultados():
     sintomas_seleccionados = {
         "Fiebre": int(check_var_fiebre.get()),
@@ -29,50 +27,81 @@ def obtener_resultados():
         "Dolor_Abdominal": int(check_var_dolor_abdominal.get()),
         "Irritabilidad": int(check_var_irritabilidad.get()),
         "Sarpullido": int(check_var_sarpullido.get()),
-        "Vomito": int(check_var_vomito.get())
+        "Vomito": int(check_var_vomito.get()),
+        "Sangrado": int(check_var_sangrado.get()),
+        "Malestar_General": int(check_var_malestar_general.get()),
+        "Dificultad_Respiratoria": int(check_var_DR.get())
+
     }
     
-    diagnostico = inferir_diagnostico(sintomas_seleccionados)
+    resultado = inferir_diagnostico(sintomas_seleccionados)
+    prob_diagnostico = resultado.values  # Extraer valores de probabilidad
     
-    resultado_label = ctk.CTkLabel(root, text=f"Diagnóstico: {diagnostico}", font=("Arial", 18))
-    resultado_label.place(x=300, y=500)
+    # Determinar el diagnóstico con mayor probabilidad
+    max_index = prob_diagnostico.argmax()
+    mensajes_diagnostico = [
+        "Sin enfermedad",
+        "Dengue sin signos de alarma",
+        "Dengue con signos de alarma",
+        "Dengue severo"
+    ]
+    mensaje_resultado = mensajes_diagnostico[max_index]
 
-# Checkbox para los síntomas
+    # Crear una ventana emergente para mostrar el resultado
+    result_window = ctk.CTkToplevel(root)
+    result_window.geometry("300x200")
+    result_window.title("Resultado del Diagnóstico")
+    result_label = ctk.CTkLabel(result_window, text=mensaje_resultado, font=("Arial", 18, "bold"))
+    result_label.pack(pady=50)
+
+# Checkbox de síntomas
 check_var_fiebre = tk.BooleanVar()
-check_box_fiebre = ctk.CTkCheckBox(root, text="Fiebre", variable=check_var_fiebre, font=("Arial", 18))
-check_box_fiebre.place(x=50, y=150)
+check_box_fiebre = ctk.CTkCheckBox(root, text="Fiebre", variable=check_var_fiebre, font=("Arial", 14))
+check_box_fiebre.place(x=50, y=100)
 
 check_var_dolor_cabeza = tk.BooleanVar()
-check_box_dolor_cabeza = ctk.CTkCheckBox(root, text="Dolor de cabeza", variable=check_var_dolor_cabeza, font=("Arial", 18))
-check_box_dolor_cabeza.place(x=50, y=200)
+check_box_dolor_cabeza = ctk.CTkCheckBox(root, text="Dolor de cabeza", variable=check_var_dolor_cabeza, font=("Arial", 14))
+check_box_dolor_cabeza.place(x=50, y=130)
 
 check_var_dolor_muscular = tk.BooleanVar()
-check_box_dolor_muscular = ctk.CTkCheckBox(root, text="Dolor muscular", variable=check_var_dolor_muscular, font=("Arial", 18))
-check_box_dolor_muscular.place(x=50, y=250)
+check_box_dolor_muscular = ctk.CTkCheckBox(root, text="Dolor muscular", variable=check_var_dolor_muscular, font=("Arial", 14))
+check_box_dolor_muscular.place(x=50, y=160)
 
 check_var_nauseas = tk.BooleanVar()
-check_box_nauseas = ctk.CTkCheckBox(root, text="Náuseas", variable=check_var_nauseas, font=("Arial", 18))
-check_box_nauseas.place(x=50, y=300)
+check_box_nauseas = ctk.CTkCheckBox(root, text="Náuseas", variable=check_var_nauseas, font=("Arial", 14))
+check_box_nauseas.place(x=50, y=190)
 
 check_var_dolor_abdominal = tk.BooleanVar()
-check_box_dolor_abdominal = ctk.CTkCheckBox(root, text="Dolor abdominal", variable=check_var_dolor_abdominal, font=("Arial", 18))
-check_box_dolor_abdominal.place(x=50, y=350)
+check_box_dolor_abdominal = ctk.CTkCheckBox(root, text="Dolor abdominal", variable=check_var_dolor_abdominal, font=("Arial", 14))
+check_box_dolor_abdominal.place(x=50, y=220)
 
 check_var_irritabilidad = tk.BooleanVar()
-check_box_irritabilidad = ctk.CTkCheckBox(root, text="Irritabilidad", variable=check_var_irritabilidad, font=("Arial", 18))
-check_box_irritabilidad.place(x=400, y=150)
+check_box_irritabilidad = ctk.CTkCheckBox(root, text="Irritabilidad", variable=check_var_irritabilidad, font=("Arial", 14))
+check_box_irritabilidad.place(x=300, y=100)
 
 check_var_sarpullido = tk.BooleanVar()
-check_box_sarpullido = ctk.CTkCheckBox(root, text="Sarpullido", variable=check_var_sarpullido, font=("Arial", 18))
-check_box_sarpullido.place(x=400, y=200)
+check_box_sarpullido = ctk.CTkCheckBox(root, text="Sarpullido", variable=check_var_sarpullido, font=("Arial", 14))
+check_box_sarpullido.place(x=300, y=130)
 
 check_var_vomito = tk.BooleanVar()
-check_box_vomito = ctk.CTkCheckBox(root, text="Vómito", variable=check_var_vomito, font=("Arial", 18))
-check_box_vomito.place(x=400, y=250)
+check_box_vomito = ctk.CTkCheckBox(root, text="Vómito", variable=check_var_vomito, font=("Arial", 14))
+check_box_vomito.place(x=300, y=160)
 
-# Botón para mostrar los resultados
-result_button = ctk.CTkButton(root, text="Resultados", font=("Arial", 20), fg_color="green", command=obtener_resultados)
-result_button.place(x=300, y=450)  # Posicionar el botón
+check_var_malestar_general = tk.BooleanVar()
+check_box_MG = ctk.CTkCheckBox(root, text="Malestar General", variable=check_var_malestar_general, font=("Arial", 14))
+check_box_MG.place(x=300, y=190)
+
+check_var_sangrado = tk.BooleanVar()
+check_box_sangrado = ctk.CTkCheckBox(root, text="Sangrado", variable=check_var_sangrado, font=("Arial", 14))
+check_box_sangrado.place(x=300, y=220)
+
+check_var_DR= tk.BooleanVar()
+check_box_DR = ctk.CTkCheckBox(root, text="Dificultad Respiratoria", variable=check_var_DR, font=("Arial", 14))
+check_box_DR.place(x=300, y=250)
+
+# Botón para obtener resultados
+result_button = ctk.CTkButton(root, text="Resultados", font=("Arial", 18), fg_color="green", command=obtener_resultados)
+result_button.place(x=240, y=300)
 
 # Iniciar la ventana
 root.mainloop()
